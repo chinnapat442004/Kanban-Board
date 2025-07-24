5f837a6cc0709396db0ffe80fa91f98303c5e01c
chinnapat442004 <chinnapat.thongthai123@gmail.com>
2025-07-25 00:40:12 +0700

    Initial layout for login and register pages

---
+++ pages/login.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const username = ref('');
+const password = ref('');
+
+const submit = () => {
+  if (username.value && password.value) {
+    console.log(username.value, password.value);
+  }
+};
+</script>
+<template>
+  <v-container
+    fluid
+    class="fill-height"
+    style="
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #f2f4f7;
+    "
+  >
+    <v-card width="500px " class="pa-4" style="border-radius: 10px">
+      <v-card-title class="text-h5" style="text-align: center"
+        >Kanban Board</v-card-title
+      >
+      <v-card-text class="text-center">
+        <p>Login</p>
+      </v-card-text>
+      <v-form fast-fail @submit.prevent @submit="submit">
+        <v-text-field
+          v-model="username"
+          placeholder="Username"
+          variant="outlined"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="password"
+          placeholder="Password"
+          type="password"
+          variant="outlined"
+        ></v-text-field>
+
+        <v-btn class="mt-2 bg-blue" type="submit" block>เข้าสู่ระบบ</v-btn>
+      </v-form>
+      <v-card-text class="text-center">
+        <p>ยังไม่มีบัญชี? <a href="/register">สมัครสมาชิก </a></p>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
---
+++ pages/register.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+const username = ref('');
+const email = ref('');
+const password = ref('');
+const confirmPassword = ref('');
+
+const submit = () => {};
+</script>
+<template>
+  <v-container
+    fluid
+    class="fill-height"
+    style="
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #f2f4f7;
+    "
+  >
+    <v-card width="500px " class="pa-4" style="border-radius: 10px">
+      <v-card-title class="text-h5" style="text-align: center"
+        >Kanban Board</v-card-title
+      >
+      <v-card-text class="text-center">
+        <p>Register</p>
+      </v-card-text>
+      <v-form fast-fail @submit.prevent @submit="submit">
+        <v-text-field
+          v-model="username"
+          placeholder="Username"
+          variant="outlined"
+        ></v-text-field>
+        <v-text-field
+          v-model="email"
+          placeholder="Email"
+          variant="outlined"
+        ></v-text-field>
+        <v-text-field
+          v-model="password"
+          placeholder="Password"
+          type="password"
+          variant="outlined"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="confirmPassword"
+          placeholder="Confirm Password"
+          type="password"
+          variant="outlined"
+        ></v-text-field>
+
+        <v-btn class="mt-2 bg-blue" type="submit" block>สมัครสมาชิก</v-btn>
+      </v-form>
+      <v-card-text class="text-center">
+        <p>มีบัญชีอยู่แล้ว? <a href="/login">เข้าสู่ระบบ</a></p>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
